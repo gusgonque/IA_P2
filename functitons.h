74f885ee2b7c4622497d803cfb4ea46356cda698
Gustavo <gustavoguguqueiroz@hotmail.com>
2024-03-01 17:20:18 -0300

    commit Queiroz 01/03

--- functitons.h
+++ functitons.h
@@ -4,27 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "C:/Program Files/Graphviz/include/graphviz/gvc.h"
+// LEMBRAR DE MUDAR O INCLUDE, CASO NECESSÁRIO
 
 typedef struct noListaAresta
 {
-    char u[10], v[10];
-    unsigned int peso;
-    struct noListaAresta *prox;
+    char v[10]; // vertices da aresta u -> v
+    unsigned int peso; // peso da aresta
+    struct noListaAresta *prox; // proximo elemento da lista
 } ListaAresta;
 
 typedef struct noListaVertice
 {
-    char u[10]; // a
-    struct noListaAresta *listaAresta;
-    struct noListaAresta *listaHeuristica;
-    struct noListaVertice *prox;
+    char u[10]; // o vertice
+    struct noListaAresta *listaAresta; // lista de arestas saindo desse vertice
+    struct noListaAresta *listaHeuristica; // lista de arestas com heuristica saindo desse vertice
+    struct noListaVertice *prox; // proximo elemento da lista
 } ListaVertice;
 
 ListaAresta *alocaAresta();
 
 ListaVertice *alocaVertice();
 
-// oi
 void desalocaAresta(ListaAresta *l);
 
 void desalocaVertice(ListaVertice *l);
--- main.c
+++ main.c
@@ -2,25 +2,25 @@
 
 void menu (){
     int x;
-    printf("Selecione um algoritmo: ");
-    printf("1 - Algoritmo 1\n"); // Todo: Implementar algoritmo 1
+    wprintf(L"Selecione um algoritmo: ");
+    wprintf(L"1 - Algoritmo 1\n"); // Todo: Implementar algoritmo 1
 
-    printf("Digite a opção desejada: ");
+    wprintf(L"Digite a opção desejada: ");
     scanf("%d", &x);
     switch (x) {
         case 1:
-            printf("Algoritmo 1 selecionado.\n");
-            break;
+            wprintf(L"Algoritmo 1 selecionado.\n");
+
         default:
-            printf("Opção inválida.\n");
+            wprintf(L"Opção inválida.\n");
             break;
     }
 }
 
 int main() {
     int x;
-    printf("Digite um número: ");
+    wprintf(L"Digite um número: ");
     scanf("%d", &x);
-    printf("O número digitado foi: %d\n", x);
+    wprintf(L"O número digitado foi: %d\n", x);
     return 0;
 }
