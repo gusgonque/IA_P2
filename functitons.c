6b7ca342f2aa429030ff2cc025c1a8b193a3c506
Gustavo <gustavoguguqueiroz@hotmail.com>
2024-03-01 16:25:48 -0300

    commit 01/03

--- functitons.c
+++ functitons.c
@@ -1,5 +1,50 @@
-\//
-// Created by Gustavo on 28/02/2024.
-//
-
 #include "functitons.h"
+
+ListaAresta *alocaAresta()
+{
+    ListaAresta *lAux = (ListaAresta *)malloc(sizeof(ListaAresta));
+    lAux->prox = NULL;
+    lAux->peso = 0;
+    memset(lAux->u, 0, sizeof(lAux->u));
+    memset(lAux->v, 0, sizeof(lAux->v));
+    return lAux;
+}
+
+ListaVertice *alocaVertice()
+{
+    ListaVertice *lAux = (ListaVertice *)malloc(sizeof(ListaVertice));
+    memset(lAux->u, 0, sizeof(lAux->u));
+    lAux->prox = NULL;
+    lAux->listaAresta = NULL;
+    lAux->listaHeuristica = NULL;
+    return lAux;
+}
+
+void desalocaAresta(ListaAresta *l)
+{
+    ListaAresta* aux;
+    while (l->prox != NULL)
+    {
+        aux = l->prox;
+        memset(l->u, 0, sizeof(l->u));
+        memset(l->v, 0, sizeof(l->v));
+        free(l);
+        l = aux;
+    }
+    free(l->u);free(l->v);
+    free(l);
+}
+
+void desalocaVertice(ListaVertice *l)
+{
+    ListaVertice *aux;
+    while (l != NULL)
+    {
+        aux = l->prox;
+        desalocaAresta(l->listaAresta);
+        desalocaAresta(l->listaHeuristica);
+        memset(l->u, 0, sizeof(l->u));
+        free(l);
+        l = aux;
+    }
+}
--- functitons.h
+++ functitons.h
@@ -1,7 +1,33 @@
 #ifndef IA_P2_FUNCTITONS_H
 #define IA_P2_FUNCTITONS_H
 
-struct
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct noListaAresta
+{
+    char u[10], v[10];
+    unsigned int peso;
+    struct noListaAresta *prox;
+} ListaAresta;
+
+typedef struct noListaVertice
+{
+    char u[10]; // a
+    struct noListaAresta *listaAresta;
+    struct noListaAresta *listaHeuristica;
+    struct noListaVertice *prox;
+} ListaVertice;
+
+ListaAresta *alocaAresta();
+
+ListaVertice *alocaVertice();
+
+void desalocaAresta(ListaAresta *l);
+
+void desalocaVertice(ListaVertice *l);
+
+
 
 #endif //IA_P2_FUNCTITONS_H
-5
